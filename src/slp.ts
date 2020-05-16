02f9a51340fa69497e68b1955dee2a38a3944117
blockparty <hello@blockparty.sh>
2020-05-16 14:19:30 -0500

    partial withdraw

--- src/slp.ts
+++ src/slp.ts
@@ -5,7 +5,10 @@ import * as bitcore from "bitcore-lib-cash";
 import * as slpjs from "slpjs";
 import * as config from "./config";
 import * as db from "./db";
+import {GrpcClient} from "grpc-bchrpc-node";
+import * as BITBOXSDK from 'bitbox-sdk';
 
+// theres just the minimum needed here to do anything
 declare module "bitcore-lib-cash" {
     export class HDPrivateKey {
       privateKey: any; // should be PrivateKey but slpjs broken typings
@@ -113,5 +116,88 @@ export function performDepositResolution(
         });
       });
     });
+  });
+}
+
+
+const client = new GrpcClient();
+const logger = console;
+const getRawTransactions = async (txids: string[]) => {
+  const getRawTransaction = async (txid: string) => {
+    console.log('get_raw_transaction', txid);
+    return await client.getRawTransaction({hash: txid, reversedHashOrder: true});
+  };
+
+  return (await Promise.all(
+    txids.map((txid: string) => getRawTransaction(txid))))
+    .map((res: any) => Buffer.from(res.getTransaction_asU8()).toString("hex"));
+};
+const validator = new slpjs.TrustedValidator({getRawTransactions, logger});
+const BITBOX = new BITBOXSDK.BITBOX();
+const network = new slpjs.BchdNetwork({BITBOX, client, validator, logger});
+
+
+export function withdraw(
+  serverId: number,
+  userId: number,
+  tokenId: string,
+  address: string,
+  amount: BigNumber
+): Promise<boolean> {
+  return new Promise(async (resolve, reject) => {
+    const token = await db.getTokenById(tokenId);
+    if (token === null) {
+      return resolve(false);
+    }
+
+    const fundingAddress           = config.fundingAddress();     // <-- must be simpleledger format
+    const fundingWif               = config.fundingWif();        // <-- compressed WIF format
+    const tokenReceiverAddress     = [ address ]; // <-- must be simpleledger format
+    const bchChangeReceiverAddress = config.fundingAddress();     // <-- must be simpleledger format
+    const tokenDecimals: number    = token.decimals;
+    const sendAmounts: BigNumber[] = [ amount.times(10**tokenDecimals) ];
+
+    const balances = await network.getAllSlpBalancesAndUtxos(fundingAddress) as slpjs.SlpBalancesResult;
+    if(balances.slpTokenBalances[tokenId] === undefined) {
+      console.error('error', 'out_of_funds');
+      return; // TODO error
+    }
+
+    const inputUtxos = balances.slpTokenUtxos[tokenId].concat(balances.nonSlpUtxos);
+    inputUtxos.forEach(txo => txo.wif = fundingWif);
+
+    const requiredNonTokenOutputs = [{
+      satoshis: 1000,
+      receiverAddress: slpjs.Utils.toCashAddress(tokenReceiverAddress[0])
+    }];
+
+    console.log('withdraw', {
+      tokenId,
+      sendAmounts,
+      inputUtxos,
+      tokenReceiverAddress,
+      bchChangeReceiverAddress,
+      requiredNonTokenOutputs
+    });
+
+    let txid = null;
+    try {
+      txid = await network.simpleTokenSend(
+        tokenId,
+        sendAmounts,
+        inputUtxos,
+        tokenReceiverAddress,
+        bchChangeReceiverAddress,
+        requiredNonTokenOutputs
+      );
+    } catch (e) {
+      console.log('send_error', e);
+      // process.exit(1);
+    }
+
+    // db.withdraw();
+
+
+    return resolve(true);
   });
 }
